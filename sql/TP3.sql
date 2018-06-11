184e62b7ca971d5bd9f1ddc0cc09d2b70f442496
Ricardo Carneiro <a15312@alunos.ipca.pt>
2018-06-11 23:12:47 +0100

    SQL BD Updated

--- sql/TP3.sql
+++ sql/TP3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 24-Maio-2018 às 21:47
+-- Generation Time: 11-Jun-2018 às 09:48
 -- Versão do servidor: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -23,16 +23,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `Administrador`
+--
+
+CREATE TABLE IF NOT EXISTS `Administrador` (
+  `ID` int(11) NOT NULL,
+  `Nome` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `Administrador`
+--
+
+INSERT INTO `Administrador` (`ID`, `Nome`, `Email`, `Password`) VALUES
+(1, 'Admin', 'user@admin.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `Aulas`
 --
 
 CREATE TABLE IF NOT EXISTS `Aulas` (
   `ID` int(11) NOT NULL,
   `Descrição` varchar(255) NOT NULL,
-  `Horario` datetime(6) NOT NULL,
+  `Start` datetime(6) NOT NULL,
+  `End` datetime(6) NOT NULL,
   `Capacidade` int(2) NOT NULL,
   `Treinador_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `Events`
+--
+
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL,
+  `start` datetime(6) NOT NULL,
+  `end` datetime(6) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `uid` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,8 +80,17 @@ CREATE TABLE IF NOT EXISTS `Mensalidades` (
   `Descrição` varchar(255) NOT NULL,
   `Data` varchar(255) NOT NULL,
   `Valor` double(8,2) NOT NULL,
-  `Status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `Status` varchar(255) NOT NULL,
+  `User_ID` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `Mensalidades`
+--
+
+INSERT INTO `Mensalidades` (`ID`, `Descrição`, `Data`, `Valor`, `Status`, `User_ID`) VALUES
+(1, 'Mensalidade Jan', '31-01-2018', 25.00, 'Pago', 19),
+(2, 'Mensalidade Jan', '31-01-2018', 25.00, 'Não Pago', 20);
 
 -- --------------------------------------------------------
 
@@ -70,7 +114,8 @@ CREATE TABLE IF NOT EXISTS `Plano_Treino` (
   `ID` int(11) NOT NULL,
   `Data` datetime(6) NOT NULL,
   `Aula_ID` int(11) NOT NULL,
-  `Treinador_ID` int(11) NOT NULL
+  `Treinador_ID` int(11) NOT NULL,
+  `Events_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -84,7 +129,15 @@ CREATE TABLE IF NOT EXISTS `Treinadores` (
   `Nome` varchar(255) NOT NULL,
   `Contacto` int(9) NOT NULL,
   `Disponibilidade` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `Treinadores`
+--
+
+INSERT INTO `Treinadores` (`ID`, `Nome`, `Contacto`, `Disponibilidade`) VALUES
+(1, 'Ana Silva', 912345678, 1),
+(2, 'Paulo Marques', 913456789, 0);
 
 -- --------------------------------------------------------
 
@@ -96,24 +149,20 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `ID` int(11) NOT NULL,
   `Nome` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
-  `Password` varchar(30) NOT NULL,
+  `Password` varchar(255) NOT NULL,
   `Contacto` int(9) NOT NULL,
   `Peso` float(10,2) NOT NULL,
   `Altura` float(5,2) NOT NULL,
-  `Pacote_ID` int(11) DEFAULT NULL,
-  `Mensalidade_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `Pacote_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `Users`
 --
 
-INSERT INTO `Users` (`ID`, `Nome`, `Email`, `Password`, `Contacto`, `Peso`, `Altura`, `Pacote_ID`, `Mensalidade_ID`) VALUES
-(2, 'Ricardo Carneiro', 'rcar@gmail.com', '123456789', 911000111, 9.99, 0.99, NULL, NULL),
-(3, 'Ricardo Carneiro', 'qwe@gmail.com', '123456789', 910001123, 9.99, 0.99, NULL, NULL),
-(4, 'Ricardo', '1234556@gmail.com', '123456789', 910001234, 100.00, 1.90, NULL, NULL),
-(5, 'Ricardo', 'asdx@hotmail.com', 'cardoso96', 123445679, 98.00, 1.67, NULL, NULL),
-(6, 'Ricardo', 'asf@gmail.com', '25f9e794323b453885f5181f1b624d', 911223187, 12.00, 1.67, NULL, NULL);
+INSERT INTO `Users` (`ID`, `Nome`, `Email`, `Password`, `Contacto`, `Peso`, `Altura`, `Pacote_ID`) VALUES
+(19, 'Ricardo', 'rcarneiropro@gmail.com', '25f9e794323b453885f5181f1b624d0b', 912388751, 76.00, 1.79, NULL),
+(20, 'Zé', 'ze@gmail.com', '25f9e794323b453885f5181f1b624d0b', 912381746, 86.00, 1.76, NULL);
 
 -- --------------------------------------------------------
 
@@ -131,16 +180,29 @@ CREATE TABLE IF NOT EXISTS `Users_Plano` (
 --
 
 --
+-- Indexes for table `Administrador`
+--
+ALTER TABLE `Administrador`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `Aulas`
 --
 ALTER TABLE `Aulas`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `Events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `Mensalidades`
 --
 ALTER TABLE `Mensalidades`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_mensalidade` (`User_ID`);
 
 --
 -- Indexes for table `Pacotes`
@@ -154,7 +216,8 @@ ALTER TABLE `Pacotes`
 ALTER TABLE `Plano_Treino`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `Treinador_ID` (`Treinador_ID`),
-  ADD KEY `FK_PersonOrder` (`Aula_ID`);
+  ADD KEY `FK_PersonOrder` (`Aula_ID`),
+  ADD KEY `Events_ID` (`Events_ID`);
 
 --
 -- Indexes for table `Treinadores`
@@ -167,8 +230,8 @@ ALTER TABLE `Treinadores`
 --
 ALTER TABLE `Users`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `Pacote_ID` (`Pacote_ID`),
-  ADD KEY `mensalidade_idfk_1` (`Mensalidade_ID`);
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `Pacote_ID` (`Pacote_ID`);
 
 --
 -- Indexes for table `Users_Plano`
@@ -182,15 +245,25 @@ ALTER TABLE `Users_Plano`
 --
 
 --
+-- AUTO_INCREMENT for table `Administrador`
+--
+ALTER TABLE `Administrador`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `Aulas`
 --
 ALTER TABLE `Aulas`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `Events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
 -- AUTO_INCREMENT for table `Mensalidades`
 --
 ALTER TABLE `Mensalidades`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `Pacotes`
 --
@@ -205,34 +278,47 @@ ALTER TABLE `Plano_Treino`
 -- AUTO_INCREMENT for table `Treinadores`
 --
 ALTER TABLE `Treinadores`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Limitadores para a tabela `Events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Plano_Treino` (`Events_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Limitadores para a tabela `Mensalidades`
 --
 ALTER TABLE `Mensalidades`
-  ADD CONSTRAINT `mensalidades_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `Users` (`Mensalidade_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_mensalidade` FOREIGN KEY (`User_ID`) REFERENCES `Users` (`ID`);
 
 --
 -- Limitadores para a tabela `Plano_Treino`
 --
 ALTER TABLE `Plano_Treino`
   ADD CONSTRAINT `FK_PersonOrder` FOREIGN KEY (`Aula_ID`) REFERENCES `Aulas` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `plano_treino_ibfk_3` FOREIGN KEY (`Treinador_ID`) REFERENCES `Treinadores` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `plano_treino_ibfk_10` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `plano_treino_ibfk_11` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `plano_treino_ibfk_3` FOREIGN KEY (`Treinador_ID`) REFERENCES `Treinadores` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `plano_treino_ibfk_4` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`),
+  ADD CONSTRAINT `plano_treino_ibfk_5` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `plano_treino_ibfk_6` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `plano_treino_ibfk_7` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`),
+  ADD CONSTRAINT `plano_treino_ibfk_8` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `plano_treino_ibfk_9` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `Users`
 --
 ALTER TABLE `Users`
-  ADD CONSTRAINT `mensalidade_idfk_1` FOREIGN KEY (`Mensalidade_ID`) REFERENCES `Mensalidade` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`Pacote_ID`) REFERENCES `Pacotes` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
